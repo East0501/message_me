d36c93c625748665858c9d2df8f10ff935b7eb8e
Kanta Azuma <azumakanta182@gmail.com>
2021-12-07 21:34:47 +0900

    Display actual messages

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+User.create(username: "Micahel", password: "password")
+User.create(username: "Mike", password: "password")
+User.create(username: "Jen", password: "password")
+User.create(username: "Dean", password: "password")
+User.create(username: "John", password: "password")
