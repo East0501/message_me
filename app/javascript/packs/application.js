d42f93ebb934708243c1af0486bcfcf4aa929a60
Kanta Azuma <azumakanta182@gmail.com>
2021-12-08 17:31:45 +0900

    Display flash message

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,6 +15,9 @@ ActiveStorage.start()
 
 
 $(document).on('turbolinks:load', function () {
-  $('.ui.dropdown')
-  .dropdown();
+  $('.ui.dropdown').dropdown();
+  $('.message .close').on('click', function() {
+    $(this).closest('.message').transition('fade');
+  });
 })
+
