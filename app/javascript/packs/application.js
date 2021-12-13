05950c875ee6c050aeb70d097857fc5df634ba73
Kanta Azuma <azumakanta182@gmail.com>
2021-12-13 12:09:36 +0900

    Edit the message form

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -14,16 +14,25 @@ Turbolinks.start()
 ActiveStorage.start()
 
 window.scroll_bottom = function() {
-  if ($('#messages').length > 0);
+  if ($('#messages').length > 0) {
     $('#messages').scrollTop($('#messages')[0].scrollHeight);
+  }
 }
 
+window.submit_message = function() {
+  $('#message_body').on('keydown', function(e) {
+    if(e.key === "Enter") {
+      e.target.value = "";
+    }
+  });
+};
 
 $(document).on('turbolinks:load', function () {
   $('.ui.dropdown').dropdown();
   $('.message .close').on('click', function() {
     $(this).closest('.message').transition('fade');
   });
+  submit_message();
   scroll_bottom();
 })
 
